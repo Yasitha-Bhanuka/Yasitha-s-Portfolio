294a596dba33eaf66e987004351f99b2ccf32981
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-11-28 10:19:19 +0530

    add hte explore me button link to google.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "726193526"
+    serviceWorkerVersion: "147348035"
   }
 });
